620fad9817e3055afa13d0abec478acef3200d90
Jacob Lord <jacoblord02@hotmail.com>
2024-11-09 13:29:36 -0700

    Testing database on movie_list page, updated css

--- view/movie_list.php
+++ view/movie_list.php
@@ -1,7 +1,3 @@
-<?php
- echo 'Test';
-?>
-
 <!DOCTYPE html>
 <head>
     <title>Movie Collection</title>
@@ -11,7 +7,29 @@
 <header><h1>Movie Collection</h1></header>
 
 <body>
-<p><?php  echo $movie_list['SERIES_TITLE']; ?> </p>
+<table>
+		<th>SERIES_TITLE</th>
+		<th>RELEASED_YEAR</th>
+		<th>RUNTIME</th>
+		<th>GENRE</th>
+		<th>IMDB_RATING</th>
+        <th>DIRECTOR</th>
+        <th>GROSS</th>
+		
+		
+		<?php foreach ($movies as $movie): ?>
+		<tr>
+
+			<td><?php echo $movie['SERIES_TITLE']; ?></td>
+			<td><?php echo $movie['RELEASED_YEAR']; ?></td>
+			<td><?php echo $movie['RUNTIME']; ?></td>
+			<td><?php echo $movie['GENRE']; ?></td>
+			<td><?php echo $movie['IMDB_RATING']; ?></td>
+            <td><?php echo $movie['DIRECTOR']; ?></td>
+            <td><?php echo $movie['GROSS']; ?></td>
+		</tr>
+		<?php endforeach; ?>
+		</table>
 </body>
 
 </html>
